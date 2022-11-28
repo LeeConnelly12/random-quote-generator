03842cf89113b513569ff59a84214918dde3da09
Lee Connelly <lee@leeconnelly.dev>
2022-11-28 20:53:05 +0000

    Return as JSON

--- main.go
+++ main.go
@@ -1,24 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"math/rand"
 	"net/http"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json")
+
 	var quotes [2]string
 	quotes[0] = "First quote."
 	quotes[1] = "Second quote."
 
 	min := 0
 	max := 2
-	fmt.Println(rand.Intn(max-min) + min)
-
 	randomIndex := rand.Intn(max-min) + min
 
-	fmt.Fprintf(w, quotes[randomIndex])
+	resp := make(map[string]string)
+	resp["quote"] = quotes[randomIndex]
+
+	jsonResp, err := json.Marshal(resp)
+
+	if err != nil {
+		log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+	}
+
+	w.Write(jsonResp)
 }
 
 func handleRequests() {
