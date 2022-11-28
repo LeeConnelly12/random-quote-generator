0e8c1c08e178d7841c295673fd78c345f259e5aa
Lee Connelly <lee@leeconnelly.dev>
2022-11-28 20:47:47 +0000

    Return a random quote

--- main.go
+++ main.go
@@ -3,12 +3,22 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage!")
-	fmt.Println("Endpoint Hit: homePage")
+	var quotes [2]string
+	quotes[0] = "First quote."
+	quotes[1] = "Second quote."
+
+	min := 0
+	max := 2
+	fmt.Println(rand.Intn(max-min) + min)
+
+	randomIndex := rand.Intn(max-min) + min
+
+	fmt.Fprintf(w, quotes[randomIndex])
 }
 
 func handleRequests() {
